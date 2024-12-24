12cdf31595aa799fab391b1f965843719d3ee961
neelima-singh07 <neelima.arun2007@gmail.com>
2024-12-24 21:18:17 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -36,6 +36,59 @@ vector<vector<int>> threeSum(vector<int>& arr) {
         
     }
 
+#4sum problem 
+
+vector<vector<int>> fourSum(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
+        vector<vector<int>>ans;
+        int n=arr.size();
+        for(int i=0;i<n-3;i++){
+            if(i>0 && arr[i]==arr[i-1])continue;
+
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && arr[j]==arr[j-1])continue;
+
+                long long sum = (long long)target - arr[i] - arr[j];
+                int s=j+1;
+                int e=n-1;
+            
+
+                while(s<e){
+                    long long y= (long long)arr[s] + arr[e];
+                    if(y==sum){
+                        ans.push_back({arr[i],arr[j],arr[s],arr[e]});
+                        s++;
+                        e--;
+                        
+
+                       while(s<e && arr[s]==arr[s-1])s++;
+                       while(s<e && arr[e]==arr[e+1])e--;
+                       
+
+
+
+
+                    }
+
+                    else if(y<sum){
+                       s++;
+
+                    }
+                    else{
+                        e--;
+                    }
+
+                }
+
+            }
+        
+
+        }
+        return ans;
+        
+    }
+
+
 
 
     
