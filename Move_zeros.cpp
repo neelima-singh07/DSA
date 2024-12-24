a74dd778d98644b504489b7f1b661c8fb0d27ec1
neelima-singh07 <neelima.arun2007@gmail.com>
2024-12-24 20:33:21 +0530

    Add files via upload

--- Move_zeros.cpp
+++ Move_zeros.cpp
@@ -25,3 +25,19 @@ public:
         
     }
 };
+
+
+#include <vector>
+using namespace std;
+
+void moveZeroes(vector<int>& arr) {
+    int j = 0; // Pointer to track position for non-zero elements
+
+    // Traverse the array
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] != 0) {
+            swap(arr[j], arr[i]); // Swap non-zero element with the element at index j
+            j++; // Move j forward
+        }
+    }
+}
