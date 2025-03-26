f6269a54603b2904a52fb1f12d10018abf5fc367
neelima-singh07 <neelima.arun2007@gmail.com>
2025-03-26 19:51:09 +0530

    Update Binary tree path sum.cpp

--- Binary tree path sum.cpp
+++ Binary tree path sum.cpp
@@ -15,3 +15,20 @@ public:
         
     }
 };
+
+
+//
+
+bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        
+        if(root==nullptr)return false;
+        
+        
+        if(root->left==nullptr &&root->right==nullptr){
+            return targetSum==0;
+        }
+        
+
+        return (hasPathSum(root->left,targetSum-root->left->val) || hasPathSum(root->right,targetSum-root->left->val)) ;
+}
